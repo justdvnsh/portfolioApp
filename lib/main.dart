f6f059baabc4986e47733cf33df79d90a4b5930e
justdvnsh <justdvnsh2208@gmail.com>
2020-03-15 23:41:48 +0530

    added container

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,62 @@ class MyApp extends StatelessWidget {
                 'Divyansh Dwivedi',
                 style: TextStyle(
                   fontSize: 36.0,
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'AI & App Developer',
+                style: TextStyle(
+                  fontSize: 18.0,
                   color: Colors.black,
                   fontWeight: FontWeight.bold
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(20.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.teal,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '+91 6394015772',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.teal,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'justdvnsh2208@gmail.com',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
